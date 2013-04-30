ac6724114bc14379306378445fa73f652f19465f
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-04-30 20:10:49 +0200

    bringing swipe functionality back to most devices

--- assets/lib/flyout/assets/coffee/flyout.coffee
+++ assets/lib/flyout/assets/coffee/flyout.coffee
@@ -4,8 +4,16 @@ $ ->
     e.preventDefault()
     $("#flyout").toggleClass "open"
   
+  ua = navigator.userAgent.toLowerCase()
+  is_android = ua.indexOf("android") > -1
+  is_webkit = ua.indexOf("safari") > -1
+  is_chrome = ua.indexOf("chrome") > -1
+  is_android_default = is_android and is_webkit and is_chrome is false
+  
   # swipe functionality
-  #$("html, body").swipe
-  #  swipeLeft: (event, direction, distance, duration, fingercount) -> $("#flyout").removeClass "open"
-  #  swipeRight: (event, direction, distance, duration, fingercount) -> $("#flyout").addClass "open"
+  if Modernizr.touch
+    unless is_android_default
+      $("html, body").swipe
+        swipeLeft: (event, direction, distance, duration, fingercount) -> $("#flyout").removeClass "open"
+        swipeRight: (event, direction, distance, duration, fingercount) -> $("#flyout").addClass "open"
       
--- js/flyout.js
+++ js/flyout.js
@@ -1,9 +1,28 @@
 (function() {
   $(function() {
-    return $("#flyout .toggle").click(function(e) {
+    var is_android, is_android_default, is_chrome, is_webkit, ua;
+
+    $("#flyout .toggle").click(function(e) {
       e.preventDefault();
       return $("#flyout").toggleClass("open");
     });
+    ua = navigator.userAgent.toLowerCase();
+    is_android = ua.indexOf("android") > -1;
+    is_webkit = ua.indexOf("safari") > -1;
+    is_chrome = ua.indexOf("chrome") > -1;
+    is_android_default = is_android && is_webkit && is_chrome === false;
+    if (Modernizr.touch) {
+      if (!is_android_default) {
+        return $("html, body").swipe({
+          swipeLeft: function(event, direction, distance, duration, fingercount) {
+            return $("#flyout").removeClass("open");
+          },
+          swipeRight: function(event, direction, distance, duration, fingercount) {
+            return $("#flyout").addClass("open");
+          }
+        });
+      }
+    }
   });
 
 }).call(this);
