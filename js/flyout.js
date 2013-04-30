253b3535325031e8e710682cb9516cfbc6b9c0b4
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-04-30 19:44:41 +0200

    corrected the flyout coffee compilation

--- js/flyout.js
+++ js/flyout.js
@@ -1,16 +1,8 @@
 (function() {
   $(function() {
-    $("#flyout .toggle").click(function(e) {
+    return $("#flyout .toggle").click(function(e) {
       e.preventDefault();
       return $("#flyout").toggleClass("open");
-    });
-    return $("html, body").swipe({
-      swipeLeft: function(event, direction, distance, duration, fingercount) {
-        return $("#flyout").removeClass("open");
-      },
-      swipeRight: function(event, direction, distance, duration, fingercount) {
-        return $("#flyout").addClass("open");
-      }
     });
   });
 
