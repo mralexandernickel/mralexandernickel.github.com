6d25bf006e596cfd7c21034ccb06ff12cfbc96d6
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-04-30 19:36:41 +0200

    outsourced the javascript for the search

--- assets/coffee/application.coffee
+++ assets/coffee/application.coffee
@@ -10,3 +10,17 @@ $ ->
   ua = navigator.userAgent.toLowerCase()
   isAndroid = ua.indexOf("android") > -1
   $("#main").append $("<pre>#{isAndroid}</pre>")
+  
+  if $("#results").length > 0
+    $.get "/search.json", (response) ->
+      map =
+        "category": getParam("category")
+        "tags": getParam("tags")
+        "search": getParam("search")
+    
+      for type,value of map
+        posts = filterPostsByPropertyValue response, type, value
+        if posts.length is 0
+          noResultsPage type, value
+        else
+          layoutResultsPage type, value, posts
--- js/application.js
+++ js/application.js
@@ -4,7 +4,29 @@
 
     ua = navigator.userAgent.toLowerCase();
     isAndroid = ua.indexOf("android") > -1;
-    return $("#main").append($("<pre>" + isAndroid + "</pre>"));
+    $("#main").append($("<pre>" + isAndroid + "</pre>"));
+    if ($("#results").length > 0) {
+      return $.get("/search.json", function(response) {
+        var map, posts, type, value, _results;
+
+        map = {
+          "category": getParam("category"),
+          "tags": getParam("tags"),
+          "search": getParam("search")
+        };
+        _results = [];
+        for (type in map) {
+          value = map[type];
+          posts = filterPostsByPropertyValue(response, type, value);
+          if (posts.length === 0) {
+            _results.push(noResultsPage(type, value));
+          } else {
+            _results.push(layoutResultsPage(type, value, posts));
+          }
+        }
+        return _results;
+      });
+    }
   });
 
 }).call(this);
